fca2c5ca0468826c355fd05b9234667451de88c8
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-02-16 20:35:58 +0800

    Added UserFragment 👌

---
+++ app/src/main/java/com/happym/mathsquare/AdminActivity.java
@@ -0,0 +1,21 @@
+package com.happym.mathsquare;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AdminActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
+    }
+
+}
